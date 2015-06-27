2c44beb5b988dc3743341887ec8a97d1dd3f6e2b
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-06-27 15:43:04 -0700

    info pannel callable

--- static/js/viewController.js
+++ static/js/viewController.js
@@ -1,9 +1,9 @@
-var infoPanel = 
-
 function openInfoPanel() {
-
+  $('#info').css('right', '600');
+  $('#info_inner').css('right', '0');
 }
 
 function closeInfoPanel() {
-
+  $('#info').css('right', '-35');
+  $('#info_inner').css('right', '-600');
 }
