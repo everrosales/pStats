788687e97517a73b82f2f71ea52572547d53b861
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-06-27 15:50:05 -0700

    search box pops out and back in when opening/closing info panel

--- static/js/viewController.js
+++ static/js/viewController.js
@@ -1,9 +1,17 @@
 function openInfoPanel() {
+  // slide info panel in.
   $('#info').css('right', '600');
   $('#info_inner').css('right', '0');
+
+  // remove shadow from search bar.
+  $('#search').css('box-shadow', 'none');
 }
 
 function closeInfoPanel() {
+  // slide info panel out.
   $('#info').css('right', '-35');
   $('#info_inner').css('right', '-600');
+
+  // add shadow to search bar.
+  $('#search').css('box-shadow', '0px 2px 5px 0px rgba(0,0,0,0.75)');
 }
