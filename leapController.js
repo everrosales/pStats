500b5467eb58e495463fd90ff7a653866b0d7b4b
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-06-27 11:46:59 -0700

    leap connected

--- leapController.js
+++ leapController.js
@@ -1,8 +1,8 @@
 var controllerOptions = { enableGestures: true };
 
 Leap.loop(controllerOptions, function(frame) {
-  var frameString = "frame:" + frame.id + 
-                    "gestures: " + frame.gestures.lenght +
+  var frameString = "frame:" + frame.id +
+                    "\ngestures: " + frame.gestures.length +
                     "\n" + frame.gestures;
-  $("body").text = frameString;
+  $("body").text(frameString);
 });
