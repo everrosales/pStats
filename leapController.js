3ccd0ba05beec05ed94ccdfa8ef52080900ef380
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-06-27 12:07:22 -0700

    gesture handler foundation

--- leapController.js
+++ leapController.js
@@ -1,8 +1,22 @@
 var controllerOptions = { enableGestures: true };
 
-Leap.loop(controllerOptions, function(frame) {
-  var frameString = "frame:" + frame.id +
-                    "\ngestures: " + frame.gestures.length +
-                    "\n" + frame.gestures;
-  $("body").text(frameString);
+var controller = Leap.loop(controllerOptions, function(frame) {
+  //handle data. maybe define new gestures.
+});
+
+controller.on("gesture", function(gesture) {
+  switch (gesture.type){
+    case "circle":
+      console.log("Circle Gesture");
+      break;
+    case "keyTap":
+      console.log("Key Tap Gesture");
+      break;
+    case "screenTap":
+      console.log("Screen Tap Gesture");
+      break;
+    case "swipe":
+      console.log("Swipe Gesture");
+      break;
+  }
 });
