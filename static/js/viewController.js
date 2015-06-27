edb0ca391c9d630d4c6066946f4f204c5ed42aea
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-06-27 16:26:49 -0700

    add bring search bar to focus

--- static/js/viewController.js
+++ static/js/viewController.js
@@ -1,6 +1,6 @@
 function openInfoPanel() {
   // slide info panel in.
-  $('#info').css('right', '600');
+  $('#info').css('right', '454');
   $('#info_inner').css('right', '0');
 
   // remove shadow from search bar.
@@ -14,4 +14,10 @@ function closeInfoPanel() {
 
   // add shadow to search bar.
   $('#search').css('box-shadow', '0px 2px 5px 0px rgba(0,0,0,0.75)');
+}
+
+function bringSearchBarToFocus() {
+  closeInfoPanel();
+  // TODO: fade everything except for search bar.
+  // TODO: bring search bar to center of screen.
 }
