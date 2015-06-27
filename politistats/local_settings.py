f92f0e4969be2d4669010f266e8788dc15e4b16c
Everardo Rosales <erosales@mit.edu>
2015-06-27 12:37:52 -0700

    Removing local db.

--- politistats/local_settings.py
+++ politistats/local_settings.py
@@ -2,12 +2,4 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        'NAME': 'politistats',
-        'USER': 'swu',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
 }
